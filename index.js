7ba2a0cd0772d8d93e70202dca20bef99a6a02b4
Maciej Mierzyński <mmierzynski@icloud.com>
2023-04-01 20:26:07 +0200

    Add index.js

--- index.js
+++ index.js
@@ -1,100 +1,104 @@
-const PORT = 8000
-const express = require('express')
-const { MongoClient } = require('mongodb')
-const {v4: uuidv4} = require('uuid')
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
-const cors = require('cors')
+const PORT = 8000;
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const { v4: uuidv4 } = require("uuid");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
 
-const uri = 'mongodb+srv://mmierzynski:zxc567@cluster0.bzfxhwx.mongodb.net/?retryWrites=true&w=majority'
+const uri =
+  "mongodb+srv://mmierzynski:zxc567@cluster0.bzfxhwx.mongodb.net/?retryWrites=true&w=majority";
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.post('/signup', async (req, res) => {
-    const client = new MongoClient(uri)
-    const { email, password, birthDate, name, location } = req.body
+app.post("/signup", async (req, res) => {
+  const client = new MongoClient(uri);
+  const { email, password, birthDate, name, location } = req.body;
 
-    const generatedUserId = uuidv4()
-    const hashedPassword = await bcrypt.hash(password, 10)
+  const generatedUserId = uuidv4();
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    try {
-        await client.connect()
-        const database = client.db('app-data')
-        const users = database.collection('users')
+  try {
+    await client.connect();
+    const database = client.db("app-data");
+    const users = database.collection("users");
 
-        const existingUser = await users.findOne({email})
+    const existingUser = await users.findOne({ email });
 
-        if (existingUser) {
-            return res.status(409).send('User already exist')
-        }
-
-        const sanitizedEmail = email.toLowerCase()
-
-        const data = {
-            user_id: generatedUserId,
-            email: sanitizedEmail,
-            hashed_password: hashedPassword,
-            name: name,
-            birth_year: birthDate,
-            location: location
-        }
-        const insertedUser = await users.insertOne(data)
-
-        const token = jwt.sign(insertedUser, sanitizedEmail, {
-            expiresIn: 60 * 24,
-        })
-
-        res.status(201).json({token, userId: generatedUserId, email: sanitizedEmail})
-    } catch (err) {
-        console.log(err)
+    if (existingUser) {
+      return res.status(409).send("User already exist");
     }
 
-})
+    const sanitizedEmail = email.toLowerCase();
 
-app.post('/login', async (req, res) => {
-    const client = new MongoClient(uri)
-    const {email, password} = req.body
+    const data = {
+      user_id: generatedUserId,
+      email: sanitizedEmail,
+      hashed_password: hashedPassword,
+      name: name,
+      birth_year: birthDate,
+      location: location,
+    };
+    const insertedUser = await users.insertOne(data);
 
-    try {
-        await client.connect()
-        const database = client.db('app-data')
-        const users = database.collection('users')
+    const token = jwt.sign(insertedUser, sanitizedEmail, {
+      expiresIn: 60 * 24,
+    });
 
-        const user = await users.findOne({email})
+    res
+      .status(201)
+      .json({ token, userId: generatedUserId, email: sanitizedEmail });
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-        const correctPassword = await bcrypt.compare(password, user.hashed_password)
+app.post("/login", async (req, res) => {
+  const client = new MongoClient(uri);
+  const { email, password } = req.body;
 
-        if (user && correctPassword) {
-            const token = jwt.sign(user, email, {
-                expiresIn: 60 * 24
-            })
-            res.status(201).json({token, userId: user.user_id})
-        }
+  try {
+    await client.connect();
+    const database = client.db("app-data");
+    const users = database.collection("users");
 
-        res.status(400).json('Invalid Credentials')
+    const user = await users.findOne({ email });
 
-    } catch (err) {
-        console.log(err)
-    } finally {
-        await client.close()
+    const correctPassword = await bcrypt.compare(
+      password,
+      user.hashed_password
+    );
+
+    if (user && correctPassword) {
+      const token = jwt.sign(user, email, {
+        expiresIn: 60 * 24,
+      });
+      res.status(201).json({ token, userId: user.user_id });
     }
-})
 
-app.get('/users', async (req, res) => {
-    const client = new MongoClient(uri)
+    res.status(400).json("Invalid Credentials");
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await client.close();
+  }
+});
 
-    try {
-        await client.connect()
-        const database = client.db('app-data')
-        const users = database.collection('users')
+app.get("/users", async (req, res) => {
+  const client = new MongoClient(uri);
 
-        const returnedUsers = await users.find().toArray()
-        res.send(returnedUsers)
-    } finally {
-        await client.close()
-    }
-})
+  try {
+    await client.connect();
+    const database = client.db("app-data");
+    const users = database.collection("users");
 
-app.listen(PORT, () => console.log('Server running on PORT ' + PORT) )
+    const returnedUsers = await users.find().toArray();
+    res.send(returnedUsers);
+  } finally {
+    await client.close();
+  }
+});
+
+app.listen(PORT, () => console.log("Server running on PORT " + PORT));
